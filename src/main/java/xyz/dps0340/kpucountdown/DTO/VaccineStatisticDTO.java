c9626c2b04528281ddf140dd8ffdc2c8f96dbc41
jiho lee <optional.int@kakao.com>
2021-09-02 10:20:40 +0900

    feat: Add ratio calculation in VaccineStatisticDTO constructor

--- src/main/java/xyz/dps0340/kpucountdown/DTO/VaccineStatisticDTO.java
+++ src/main/java/xyz/dps0340/kpucountdown/DTO/VaccineStatisticDTO.java
@@ -3,6 +3,7 @@ package xyz.dps0340.kpucountdown.DTO;
 import lombok.Getter;
 import lombok.Setter;
 import xyz.dps0340.kpucountdown.Entity.VaccineStatisticEntity;
+import xyz.dps0340.kpucountdown.GlobalVariable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,11 +15,15 @@ public class VaccineStatisticDTO {
     private Long totalFirstCnt;
     private Long totalSecondCnt;
     private LocalDateTime expectedMeetingDate;
+    private Double firstRatio;
+    private Double secondRatio;
 
     public VaccineStatisticDTO(VaccineStatisticEntity entity) {
         this.date = entity.getDate();
         this.totalFirstCnt = entity.getTotalFirstCnt();
         this.totalSecondCnt = entity.getTotalSecondCnt();
         this.expectedMeetingDate = entity.getExpectedMeetingDate();
+        this.firstRatio = 100.0 * this.totalFirstCnt / GlobalVariable.NATIONAL_POPULATION;
+        this.secondRatio = 100.0 * this.totalSecondCnt / GlobalVariable.NATIONAL_POPULATION;
     }
 }
