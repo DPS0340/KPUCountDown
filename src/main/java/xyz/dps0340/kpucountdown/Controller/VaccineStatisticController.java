e04d67edd3a00cfbac3765239b7028d9836067c8
jiho lee <optional.int@kakao.com>
2021-09-02 17:14:22 +0900

    refactor: Shorten variable

--- src/main/java/xyz/dps0340/kpucountdown/Controller/VaccineStatisticController.java
+++ src/main/java/xyz/dps0340/kpucountdown/Controller/VaccineStatisticController.java
@@ -4,29 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.dps0340.kpucountdown.Entity.VaccineStatisticEntity;
-import xyz.dps0340.kpucountdown.Repository.VaccineStatisticRepository;
 import xyz.dps0340.kpucountdown.DTO.VaccineStatisticDTO;
 import xyz.dps0340.kpucountdown.Service.VaccineStatisticService;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class VaccineStatisticController {
     @Autowired
-    private VaccineStatisticService vaccineStatisticService;
+    private VaccineStatisticService service;
 
     @GetMapping("/stat")
     public List<VaccineStatisticDTO> getVaccineStats() {
-        return vaccineStatisticService.getStats();
+        return service.getStats();
     }
 
     @GetMapping("/stat/today")
     public VaccineStatisticDTO getTodayVaccineStat() {
-        return vaccineStatisticService.getTodayStat();
+        return service.getTodayStat();
     }
 
     @GetMapping(
@@ -34,6 +29,7 @@ public class VaccineStatisticController {
             produces = MediaType.IMAGE_PNG_VALUE
     )
     public byte[] getVaccineStatsGraph() {
-        return vaccineStatisticService.getVaccineStatsGraph();
+        return service.getVaccineStatsGraph();
     }
+
 }
